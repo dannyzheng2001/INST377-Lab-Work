1f252ac792d9b5a03e09ae84181b909c3949ee06
dannyzheng427 <dannylobby123@gmail.com>
2023-04-15 17:40:12 -0400

    lab 8 final product commit

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -36,11 +36,36 @@ function curRestaurantList(list) {
   }));
 }
 
+function initMap() {
+  const carto = L.map('map').setView([38.98, -76.93], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(carto);
+  return carto;
+}
+
+function markerPlace(array, map) {
+  console.log("array for markers");
+
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item) => {
+    console.log("marker place", item);
+    const {coordinates} = item.geocoded_column_1;
+    L.marker([coordinates[1], coordinates[0]]).addTo(map)
+  })
+}
+
 async function mainEvent() {
   // the async keyword means we can make API requests
   const mainForm = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
-  // const filterButton = document.querySelector(".filter_button");
   const loadDataButton = document.querySelector("#data_load");
+  const clearDataButton = document.querySelector("#data_clear");
   const generateListButton = document.querySelector("#generate");
   const textField = document.querySelector("#resto");
   
@@ -48,12 +73,14 @@ async function mainEvent() {
   loadAnimation.style.display = "none";
   generateListButton.classList.add("hidden");
 
+  const carto = initMap();
+
   const storedData = localStorage.getItem('storedData');
-  const parsedData = JSON.parse(storedData);
+  let parsedData = JSON.parse(storedData);
 
 
 
-  if (parsedData !== null && parsedData.length > 0) {
+  if (parsedData?.length > 0) {
     console.log(parsedData.length);
     generateListButton.classList.remove("hidden");
   }
@@ -71,11 +98,13 @@ async function mainEvent() {
     );
 
     const storedList = await results.json();
-    if (storedList.length > 0) {
+    localStorage.setItem('storedData', JSON.stringify(storedList));
+    parsedData = storedList;
+
+    if (parsedData?.length > 0) {
       generateListButton.classList.remove("hidden");
     }
     // console.log(storedList);
-    localStorage.setItem('storedData', JSON.stringify(storedList));
 
     
 
@@ -91,6 +120,7 @@ async function mainEvent() {
     currentList = curRestaurantList(parsedData);
     console.log(currentList);
     injectHTML(currentList);
+    markerPlace(currentList, carto);
   });
 
   textField.addEventListener("input", (event) => {
@@ -98,7 +128,14 @@ async function mainEvent() {
     const newList = filterList(currentList, event.target.value);
     console.log(newList);
     injectHTML(newList);
+    markerPlace(newList, carto);
   });
+
+  clearDataButton.addEventListener("click", (event) => {
+    console.log("clear browser data");
+    localStorage.clear();
+    console.log("localStorage Check", localStorage.getItem("storedData"));
+  })
 }
 
 /*
